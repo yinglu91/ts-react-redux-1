755fc5a65a317cc56d276a559362c17ffea4ed2c
ylu <ylu@ert.com>
2021-01-01 12:32:50 -0500

    done fetch  data in redux part

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,16 @@
 import { combineReducers } from 'redux'
+import { todosReducer } from './todos'
+import { Todo } from '../actions'
 
-export const reducers = combineReducers({
-  counter: () => 1
+// entire redux state
+export interface StoreState {
+  todos: Todo[]
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer
 })
+
+// {
+//   todos: [Todo, Todo, Todo]
+// }
